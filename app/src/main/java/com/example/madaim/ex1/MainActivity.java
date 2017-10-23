817eadc6d74aad7f5ab07c235ac2a1b83896dc4b
Madaim <yarden2001@hotmail.com>
2017-10-23 17:47:49 +0300

    abashelshay

--- app/src/main/java/com/example/madaim/ex1/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex1/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout1);
+        setContentView(R.layout.layout2);
         //aba shel shayfdfdsgf
     }
 }
